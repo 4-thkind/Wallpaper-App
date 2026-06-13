04b912fec3ecf51b80a787b356e4fbc6567c8b96
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-06-13 10:54:56 +0000

    added set button

--- WallpaperApp.py
+++ WallpaperApp.py
@@ -26,3 +26,8 @@ next_button=Button(root,text="Next wallpaper",fg="black",bg="yellow",font=("cali
                    highlightcolor="blue",highlightthickness=1,
                    command=change_wall)
 next_button.pack(ipady=5,ipadx=2,pady=(0,5))
+
+set_button=Button(root,text="Set wallpaper",fg="black",bg="yellow",font=("calibri",14,"bold"),relief="solid",
+                   highlightcolor="blue",highlightthickness=1,
+                   command=set_wall)
+set_button.pack(ipady=5,ipadx=3,pady=(0,20))
