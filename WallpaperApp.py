d9986009220a64144598ecf89c9e29b5dcb6dc2f
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-06-13 10:50:33 +0000

    linked the wallpaper directory with app

--- WallpaperApp.py
+++ WallpaperApp.py
@@ -8,3 +8,9 @@ root=Tk()
 root.title("Wallpaper App")
 root.geometry("400x600")
 root.configure(bg="red")
+
+files=os.listdir("wallpaperProto images")
+img_arr=[]
+
+for file in files:
+    img_arr.append(ImageTk.PhotoImage(Image.open(os.path.join("wallpaperProto images",file)).resize((400,300))))
