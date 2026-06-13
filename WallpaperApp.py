0afecc6e3e970e826df7185801980e5486b9250b
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-06-13 10:52:00 +0000

    inserted the static wallpaper

--- WallpaperApp.py
+++ WallpaperApp.py
@@ -14,3 +14,10 @@ img_arr=[]
 
 for file in files:
     img_arr.append(ImageTk.PhotoImage(Image.open(os.path.join("wallpaperProto images",file)).resize((400,300))))
+
+text_label=Label(root,text="Select Wallpaper",font=("calibri",30,"bold"),fg="black",bg="red")
+text_label.pack(pady=(50,10))
+
+img_lable=Label(root,image=img_arr[0])
+img_lable.pack(pady=(10,20))
+img_lable.config(relief="solid",highlightbackground="black",highlightthickness=2)
